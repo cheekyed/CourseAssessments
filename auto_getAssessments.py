0cabbf20db13cab5ba37d2318dc84ce2906cc9eb
Edwin <Edwin@example.com>
2019-02-28 18:46:58 +1100

    created up to getHtml method

--- auto_getAssessments.py
+++ auto_getAssessments.py
@@ -1,6 +1,48 @@
-import urllib2
+from urllib.request import urlopen
 import sys
+import socket 
 from bs4 import BeautifulSoup
 
-courseUrls []
+courseUrlList = []
+pageHtmlList = []
+maintenanceUrl = "http://maintenance.business.unsw.edu.au/#assessment"
 
+
+def addUrls():
+	for i in range (1,len(sys.argv)):
+		courseUrl = 'https://www.business.unsw.edu.au/degrees-courses/course-outlines/archives/' + sys.argv[i] + '#assessment'
+		courseUrlList.append(courseUrl)
+		return
+
+def internet_on():
+	try:
+		socket.create_connection(("www.google.com", 80))
+		print('internet connection is on')
+	except OSError:
+		print('no internet connection')
+		sys.exit()
+	return
+
+def getHtml():
+	for url in courseUrlList:
+		page = urlopen(url)
+		if page.geturl() == maintenanceUrl:
+			print('page is under maintenance')
+			sys.exit()
+		else:
+			pageHtml = BeautifulSoup(page, 'html.parser')
+			pageHtmlList.append(pageHtml)
+		print('page html successfully acquired')
+		return
+
+def main ():
+	addUrls()
+	internet_on()
+	getHtml()
+ 
+
+
+
+
+if __name__ == '__main__':
+	main()
